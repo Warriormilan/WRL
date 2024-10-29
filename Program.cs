2c2353f9f065ed25f2c724198cf1f29587806b16
Warriormilan <144992584+Warriormilan@users.noreply.github.com>
2024-10-29 13:08:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите количество месяцев");
+int n= Convert.ToInt32(Console.ReadLine());
+
+DateTime SD = new DateTime(1998, 1, 1);
+DateTime RD = SD.AddMonths(n).AddDays(2);
+
+string NoY = RD.ToString("dddd");
+string NoM = RD.ToString("MMMM");
+int year  = RD.Year;
+int day = RD.Day;
+bool LY = DateTime.IsLeapYear(year);
+
+Console.WriteLine($"день, месяц, год, день недели полученного года: {day} {NoM} {year} {NoY}");
+Console.WriteLine($"является ли полученный год високоснымk: {(LY ? "да" : "нет")}");
